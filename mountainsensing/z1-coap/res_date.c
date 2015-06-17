40e60766fbcf38b9a698bdda250c999213e051b2
kmartinez <km@ecs.soton.ac.uk>
2015-06-17 13:43:00 +0100

    post date implemented

--- mountainsensing/z1-coap/res_date.c
+++ mountainsensing/z1-coap/res_date.c
@@ -24,5 +24,34 @@ static void res_get_handler(void* request, void* response, uint8_t *buffer, uint
     REST.set_response_payload(response, buffer, length);
 }
 
-RESOURCE(res_date, "title=\"date: ?len=0..\";rt=\"Text\"", res_get_handler, NULL, NULL, NULL);
+static void res_post_put_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+  size_t len = 0;
+  int success = 1;
+  static uint8_t y, mo, d, h, mi, se;
+  const char *txt = NULL;
+
+
+	REST.get_post_variable(request, "y", &txt);
+	y = atoi(txt);
+	REST.get_post_variable(request, "mo", &txt);
+	mo = atoi(txt);
+	REST.get_post_variable(request, "d", &txt);
+	d = atoi(txt);
+	REST.get_post_variable(request, "h", &txt);
+	h = atoi(txt);
+	REST.get_post_variable(request, "mi", &txt);
+	mi = atoi(txt);
+	REST.get_post_variable(request, "se", &txt);
+	se = atoi(txt);
+	/* write the values to the RTC */
+	set_time(y, mo, d, h, mi, se);
+
+	/* should check failure in the above - at the moment its always OK*/
+  if(!success) {
+    REST.set_response_status(response, REST.status.BAD_REQUEST);
+  }
+}
+
+RESOURCE(res_date, "title=\"date: ?len=0..\";rt=\"Text\"", res_get_handler, res_post_put_handler, NULL, NULL);
 
