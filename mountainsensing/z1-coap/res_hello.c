0d44dc83088dfb2002499b12d9e24634010b352c
kmartinez <km@ecs.soton.ac.uk>
2015-06-17 14:12:07 +0100

    hello returns batV now

--- mountainsensing/z1-coap/res_hello.c
+++ mountainsensing/z1-coap/res_hello.c
@@ -4,6 +4,9 @@
  */
 #include "er-server.h"
 #include "rest-engine.h"
+#include "sampler.h"
+#include "dev/batv-sensor.h" // Batt
+
 
 /*
  * A handler function named [resource name]_handler must be implemented for each RESOURCE.
@@ -11,10 +14,18 @@
  * preferred_size and offset, but must respect the REST_MAX_CHUNK_SIZE limit for the buffer.
  * If a smaller block size is requested for CoAP, the REST framework automatically splits the data.
  */
-void res_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
-    char const * const message = "Hello World!";
-    int length = 12;
+static void res_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
+    //char const * const message = "Hello World!";
+    char message[30];
+    int length;
+	float f;
 
+	ms1_sense_on();
+
+	f = get_sensor_batt();
+	ms1_sense_off();
+	sprintf(message,"node batV %d.%d\n",(int)f,(int)(f * 1000) );
+	length = strlen(message);
     memcpy(buffer, message, length);
 
     REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
