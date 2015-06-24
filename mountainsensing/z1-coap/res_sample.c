ddbed75a6c1764e72e90cb47dbcf206150b04ec1
Arthur Fabre <arthur@arthurfabre.com>
2015-06-24 11:29:02 +0100

    Fix off by one error in res_sample
    The *offset point is input/ouput field, and it was used as an input
    after being writen too. This caused an incorrect offset to be used when
    copying the response into the final buffer.

--- mountainsensing/z1-coap/res_sample.c
+++ mountainsensing/z1-coap/res_sample.c
@@ -12,6 +12,7 @@
 #include "pb_encode.h"
 #include "readings.pb.h"
 #include "store.h"
+#include <inttypes.h>
 
 #define DEBUG_ON
 #include "debug.h"
@@ -22,30 +23,33 @@
  */
 static void res_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
     static Sample sample;
+    static int32_t current_offset;
     static uint8_t pb_buffer[Sample_size];
     static pb_ostream_t pb_ostream;
     static uint8_t buffer_len;
 
     DEBUG("Serving request! Offset %d, PrefSize %d\n", (int) *offset, preferred_size);
 
+    current_offset = *offset;
+
     // Only get data if this is the first request of a blockwise transfer
-    if (*offset == 0 && !store_get_latest_sample(&sample)) {
+    if (current_offset == 0 && !store_get_latest_sample(&sample)) {
         // 500 internal error
         DEBUG("Unable to get sample!\n");
         REST.set_response_status(response, REST.status.INTERNAL_SERVER_ERROR);
         return;
     }
 
-    DEBUG("Got sample with id %d\n", sample.id);
+    DEBUG("Got sample with id %" PRIu32  "\n", sample.id);
 
     pb_ostream = pb_ostream_from_buffer(pb_buffer, sizeof(pb_buffer));
     pb_encode_delimited(&pb_ostream, Sample_fields, &sample);
 
     // If whatever we have to send fits in one block, just send that
-    if (pb_ostream.bytes_written - *offset <= preferred_size) {
+    if (pb_ostream.bytes_written - current_offset <= preferred_size) {
         DEBUG("Request fits in a single block\n");
 
-        buffer_len = pb_ostream.bytes_written - *offset;
+        buffer_len = pb_ostream.bytes_written - current_offset;
 
         // Indicates this is the last chunk
         *offset = -1;
@@ -59,7 +63,7 @@ static void res_get_handler(void* request, void* response, uint8_t *buffer, uint
         *offset += preferred_size;
     }
 
-    memcpy(buffer, pb_buffer + *offset, buffer_len);
+    memcpy(buffer, pb_buffer + current_offset, buffer_len);
 
     REST.set_header_content_type(response, REST.type.APPLICATION_OCTET_STREAM);
     REST.set_response_payload(response, buffer, buffer_len);
