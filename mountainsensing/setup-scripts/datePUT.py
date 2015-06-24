4b5f9608a732de1b98153b7e7a0df878550e9d78
kmartinez <km@ecs.soton.ac.uk>
2015-06-24 09:33:25 +0100

    incomplete date POST to node

--- mountainsensing/setup-scripts/datePUT.py
+++ mountainsensing/setup-scripts/datePUT.py
@@ -11,29 +11,33 @@ logging.basicConfig(level=logging.INFO)
 
 @asyncio.coroutine
 def main():
-	"""
-	gets broken-down date tiem and POSTs it 
-	"""
-	context = yield from Context.create_client_context()
+    """
+    gets broken-down date-time and POSTs it to set the node rtc
+    """
+    context = yield from Context.create_client_context()
 
 #    yield from asyncio.sleep(2)
 
-	now = datetime.datetime.now()
-	y = now.year - 2000
-	m = now.month
-	d = now.day
-	h = now.hour
-	print("min= %d" % now.minute )
-	print("sec= %d" % now.second )
-	payload = b"y
-	payload = b"The quick brown fox jumps over the lazy dog.\n" * 30
-	request = Message(code=PUT, payload=payload)
-	request.opt.uri_host = '127.0.0.1'
-	request.opt.uri_path = ("other", "block")
+    now = datetime.datetime.now()
+    y = now.year
+    m = now.month
+    d = now.day
+    h = now.hour
+    mi = now.minute
+    se = now.second
 
-	response = yield from context.request(request).response
+    payload = b"y"
+    #payload = b"The quick brown fox jumps over the lazy dog.\n" * 30
+# not sure if we need a payload!
+    request = Message(code=POST, payload=payload)
+    request.opt.uri_host = 'aaaa::c30c:0:0:1058'
+    URI = "/date?y=%d&mo=%d&d=%d&h=%d&mi=%d&se=%d" % (y,m,d,h,mi,se)
+    print("uri: %s\n" % URI)
+    request.opt.uri_path = (URI, "block")
 
-	print('Result: %s\n%r'%(response.code, response.payload))
+    response = yield from context.request(request).response
+
+    print('Result: %s\n%r'%(response.code, response.payload))
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
