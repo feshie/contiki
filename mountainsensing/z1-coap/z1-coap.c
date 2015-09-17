75a5657f6426f16385dd621af506ed4c8d18d8ff
Arthur Fabre <arthur@arthurfabre.com>
2015-09-17 18:18:47 +0100

    Update docs for z1-coap
    We still had the copyright header left from the websense application,
    but we share no code with them now - everything has been rewritten.

--- mountainsensing/z1-coap/z1-coap.c
+++ mountainsensing/z1-coap/z1-coap.c
@@ -1,44 +1,16 @@
-/*
- * Based on Z1-Websense, which has the following licence:
- *
- * Copyright (c) 2011, Zolertia(TM) is a trademark by Advancare,SL
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- */
-
 /**
  * \file
- *         adapted from Battery and Temperature IPv6 Demo for Zolertia Z1
+ *          Sampler for the mountain-sensing platform.
+ *
+ *          This intializes the store, and starts the er_server, and the sampler.
+ *
  * \author
  *          Dan Playle      <djap1g12@soton.ac.uk>
  *          Philip Basford  <pjb@ecs.soton.ac.uk>
  *          Graeme Bragg    <gmb1g08@ecs.soton.ac.uk>
  *          Tyler Ward      <tw16g08@ecs.soton.ac.uk>
  *          Kirk Martinez   <km@ecs.soton.ac.uk>
+ *          Arthur Fabre    <af1g12@ecs.soton.ac.uk>
  */
 
 #include "contiki.h"
