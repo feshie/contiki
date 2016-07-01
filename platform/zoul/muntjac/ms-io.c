79769670959b22cfc891be6c064557d2476a1fc1
Arthur Fabre <arthur@arthurfabre.com>
2016-07-01 15:43:39 +0100

    Muntjac: Add RS485 / avr-handler support
    Add support for reading data from RS485 sensors using the avr-handler.c
    Actually do this in the ms_extra function used by the sampler.

--- platform/zoul/muntjac/ms-io.c
+++ platform/zoul/muntjac/ms-io.c
@@ -1,12 +1,35 @@
 #include <time.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include "board.h"
 #include "contiki.h"
 #include "ms-io.h"
+#include "sampler.h"
 #include "ds3231-sensor.h"
+#include "dev/avr-handler.h"
+#include "lpm.h"
 
 #define DEBUG_ON
 #include "debug.h"
+
+/**
+ * Pointer to the sample we're currently working on.
+ * Used by the extra callback.
+ */
+static Sample *sample_extra;
+
+/**
+ * Struct used to get data from an AVR
+ */
+static struct avr_data data = {
+    // Size of the buffer is the size of the buffer in the Sample
+    .size = sizeof(((Sample_AVR_t *)0)->bytes)
+};
+
+/**
+ *
+ */
+static void extra_callback(bool isSuccess);
 
 /**
  * Read the temperature.
@@ -16,15 +39,23 @@
 static bool get_temp(float *temp);
 
 void ms_init(void) {
-    // TODO
+    // Set the AVR callback
+    avr_set_callback(&extra_callback);
 }
 
 void ms_sense_on(void) {
-	// TODO
+    lpm_set_max_pm(LPM_PM0);
+
+    // Enable sense
+    GPIO_SET_PIN(GPIO_PORT_TO_BASE(PWR_SENSE_EN_PORT), GPIO_PIN_MASK(PWR_SENSE_EN_PIN));
 }
 
 void ms_sense_off(void) {
-    // TODO
+    // TODO - Save and restore the max_pm in ms_sense_on
+    //lpm_set_max_pm(LPM_CONF_MAX_PM);
+
+    // Disable sense
+    GPIO_CLR_PIN(GPIO_PORT_TO_BASE(PWR_SENSE_EN_PORT), GPIO_PIN_MASK(PWR_SENSE_EN_PIN));
 }
 
 bool ms_get_time(uint32_t *seconds) {
@@ -58,11 +89,47 @@ bool ms_set_time(uint32_t seconds) {
 }
 
 bool ms_get_extra(Sample *sample, SensorConfig *config) {
+	sample_extra = sample;
+
+    ms_sense_on();
 
     sample->has_temp = get_temp(&sample->temp);
 
-    // Everything completed synchronously
-    return true;
+    // If there are no avrs, we're done
+    if (config->avrIDs_count < 1) {
+        // No need to wait on anything else
+        return true;
+    }
+
+    GPIO_SET_PIN(GPIO_PORT_TO_BASE(PWR_SENSE_EN_PORT), GPIO_PIN_MASK(PWR_SENSE_EN_PIN));
+
+    uint8_t avr_id = (uint8_t) config->avrIDs[0];
+
+    // Use the buffer in the sample directly
+    data.data = sample->AVR.bytes;
+    data.len = &sample->AVR.size;
+    data.id = avr_id;
+
+    DEBUG("Getting data from avr %x\n", avr_id);
+
+	if (avr_get_data(&data)) {
+        return false;
+    } else {
+        ms_sense_off();
+        return true;
+    }
+}
+
+void extra_callback(bool isSuccess) {
+    DEBUG("Sampled from avr. Success: %d\n", isSuccess);
+
+    GPIO_CLR_PIN(GPIO_PORT_TO_BASE(PWR_SENSE_EN_PORT), GPIO_PIN_MASK(PWR_SENSE_EN_PIN));
+
+    sample_extra->has_AVR = isSuccess;
+
+    ms_sense_off();
+
+    sampler_extra_performed();
 }
 
 bool get_temp(float *temp) {
